bc154821cf7c622b39ea604f736e49c1eb2b8950
Christian Fischer <cf35@live.de>
2022-06-13 21:10:17 +1000

    Initial Commit

---
+++ KeyPressed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeyPressed
+//
+//  Created by Christian Fischer on 13.06.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
